ee8981a6145f61e935519d30a21642f79fb61e00
Agent-grey <markkibuthu@gmail.com>
2024-10-21 11:41:51 +0300

    complete lab

--- server/app.py
+++ server/app.py
@@ -28,16 +28,38 @@ class Signup(Resource):
         return user.to_dict(), 201
 
 class CheckSession(Resource):
-    pass
+    def get(self):
+        """Check if a user is logged in."""
+        user_id = session.get('user_id')
+        if user_id:
+            user = User.query.get(user_id)
+            return user.to_dict(), 200
+        return {}, 204  
+
 
 class Login(Resource):
-    pass
+    def post(self):
+        """Log a user in."""
+        json = request.get_json()
+        user = User.query.filter_by(username=json['username']).first()
+        
+        if user and user.authenticate(json['password']):
+            session['user_id'] = user.id
+            return user.to_dict(), 200
+        return {"error": "Invalid credentials"}, 401
 
 class Logout(Resource):
-    pass
+    def delete(self):
+        """Log a user out."""
+        session.clear()  
+        return {}, 204  
+
 
 api.add_resource(ClearSession, '/clear', endpoint='clear')
 api.add_resource(Signup, '/signup', endpoint='signup')
+api.add_resource(CheckSession, '/check_session', endpoint='check_session')
+api.add_resource(Login, '/login', endpoint='login')
+api.add_resource(Logout, '/logout', endpoint='logout')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
